c3f144d4b9c22ef34b3a7b386f8dbc4a1e4abfeb
nbhalala <42828619+nbhalala@users.noreply.github.com>
2022-11-16 15:17:15 -0700

    Update database.go

--- api/database/database.go
+++ api/database/database.go
@@ -8,6 +8,13 @@ import(
 
 var Ctx = context.Background()
 
+/*
+ * NewClient(): This method takes a struct with the properties to connect to the Redis Server.
+ * Addr : This describes the address and port to the Redis server instance.
+ * Password : Password to the Redis instance.
+ * DB : The database index to use for the application.
+*/
+
 func CreateClient(dbNo int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr: os.Getenv("APP_DB_ADDRESS"),
