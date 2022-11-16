20c3081a3242f2f35094011ea2ec78c03b14c608
nbhalala <42828619+nbhalala@users.noreply.github.com>
2022-11-16 13:52:36 -0700

    Update main.go

--- api/main.go
+++ api/main.go
@@ -6,7 +6,7 @@ import(
 	"os"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/nbhalala/codingtask/routes"
+	"github.com/nbhalala/codingtask/api/routes"
 	"github.com/joho/godotenv"
 )
 
