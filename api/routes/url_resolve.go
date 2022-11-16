37f7471b9a7b23fb2bd207943539b9ecf980fac3
nbhalala <42828619+nbhalala@users.noreply.github.com>
2022-11-16 15:08:13 -0700

    Update url_resolve.go

--- api/routes/url_resolve.go
+++ api/routes/url_resolve.go
@@ -9,17 +9,17 @@ import(
 func urlResolve(c *fiber.Ctx) error {
 	url := c.Params("url")
 
-	r := database.createClient(0)
+	r := database.CreateClient(0)
 	defer r.Close()
 
 	value, err := r.Get(database.Ctx, url).Result()
 	if err == redis.Nil{
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"Short not found in the Database."})
-	} else if err != nil {
-		return c.Status(fiber.StatusInternalError).JSON(fiber.Map{"error":"Cannot connect to the Database."})
-	}
+	} //else if err != nil {
+	//	return c.Status(fiber.StatusInternalError).JSON(fiber.Map{"error":"Cannot connect to the Database."})
+	//}
 
-	rInr := database.createClient(1)
+	rInr := database.CreateClient(1)
 	defer rInr.Close()
 
 	_ = rInr.Incr(database.Ctx, "counter")
