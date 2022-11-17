9e4d1b020e67a69357a2ca523f0aa29daac931e3
nbhalala <42828619+nbhalala@users.noreply.github.com>
2022-11-16 22:13:12 -0700

    Update database.go

--- api/database/database.go
+++ api/database/database.go
@@ -1,3 +1,8 @@
+/*
+ * Database: Radis
+ * Radis is an in-memory data structure store, used as a distributed, in-memory key–value database, cache and message broker, with optional durability.
+ */
+
 package database
 
 import (
